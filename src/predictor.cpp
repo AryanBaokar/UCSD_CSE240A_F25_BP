7ab161edf4d2036ca3939aeb9c877c53a6ae821e
Aryan Baokar <aryanbaokar77@gmail.com>
2025-11-03 11:46:13 -0800

    Custom added, tourney still off by few points

--- src/predictor.cpp
+++ src/predictor.cpp
@@ -33,6 +33,10 @@ int verbose;
 uint8_t a21264_localHistoryBits = 10;   // Number of bits for local history of each branch
 uint8_t a21264_globalHistoryBits  = 12; // Number of bits for global history
 
+// Custom Predictor Config
+uint8_t custom_localHistoryBits = 12;   // Number of bits for local history of each branch
+uint8_t custom_globalHistoryBits  = 12; // Number of bits for global history
+
 //------------------------------------//
 //      Predictor Data Structures     //
 //------------------------------------//
@@ -46,11 +50,17 @@ uint64_t ghistory;
 
 // Alpha 21264 Tournament predictor
 uint16_t a21264_globalHistory;   // Actual history of global
-uint16_t a21264_globalHistoryBeforeTrain;
 uint16_t* a21264_localHistory;   // Actual history of each branch, use pc to index this
 uint8_t* a21264_ght;             // Table containing predictions for global history
 uint8_t* a21264_localPredTable;  // Prediction to be made for each type of local history
 uint8_t* a21264_chooser;         // Chooser function
+
+// Custom predictor
+uint16_t  custom_globalHistory;   // Actual history of global
+uint16_t* custom_localHistory;   // Actual history of each branch, use pc to index this
+uint8_t*  custom_ght;             // Table containing predictions for global history
+uint8_t*  custom_localPredTable;  // Prediction to be made for each type of local history
+uint8_t*  custom_chooser;         // Chooser function
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -157,14 +167,14 @@ void a21264_init()
 
   for(int i = 0; i < a21264_lhtEntries; i++)
   {
-    a21264_localHistory[i] = 0;
+    a21264_localHistory[i] = 0xAAAA;
   }
 
   a21264_chooser = (uint8_t *)malloc(a21264_ghtEntries * sizeof(uint8_t));
 
   for(int i = 0; i < a21264_ghtEntries; i++)
   {
-    a21264_chooser[i] = 0;
+    a21264_chooser[i] = 1;
   }
 
 }
@@ -173,6 +183,7 @@ uint8_t a21264_predictGlobal()
 {
   uint16_t a21264_ghtEntries  = 1 << a21264_globalHistoryBits;
   uint16_t index = a21264_globalHistory & (a21264_ghtEntries - 1);
+  
   switch (a21264_ght[index])
   {
   case WN:
@@ -246,7 +257,6 @@ uint8_t a21264_trainGlobal(uint8_t outcome)
   // Training Global Prediction
   uint16_t a21264_ghtEntries  = 1 << a21264_globalHistoryBits;
   uint16_t index = a21264_globalHistory & (a21264_ghtEntries - 1);
-  a21264_globalHistoryBeforeTrain = a21264_globalHistory;
   uint8_t retVal;
 
   // Update state of entry in bht based on outcome
@@ -357,11 +367,11 @@ uint8_t a21264_trainLocal(uint32_t pc , uint8_t outcome)
 
 void a21264_train(uint32_t pc, uint8_t outcome)
 {
+  uint16_t a21264_ghtEntries  = 1 << a21264_globalHistoryBits;
+  uint16_t index = a21264_globalHistory & (a21264_ghtEntries - 1);
+
   uint8_t globTrained = a21264_trainGlobal(outcome);
   uint8_t localTrained = a21264_trainLocal(pc, outcome);
-
-  uint16_t a21264_ghtEntries  = 1 << a21264_globalHistoryBits;
-  uint16_t index = a21264_globalHistoryBeforeTrain & (a21264_ghtEntries - 1);
 
   if(globTrained == localTrained)
   {
@@ -383,8 +393,6 @@ void a21264_train(uint32_t pc, uint8_t outcome)
   }
 }
 
-
-
 void a21264_free()
 {
   free(a21264_ght);
@@ -392,6 +400,266 @@ void a21264_free()
   free(a21264_localPredTable);
   free(a21264_chooser);
 }
+
+void custom_init()
+{
+  uint16_t custom_ghtEntries  = 1 << custom_globalHistoryBits;
+  custom_ght = (uint8_t *)malloc(custom_ghtEntries * sizeof(uint8_t));
+
+  for(int i = 0; i < custom_ghtEntries; i++)
+  {
+    custom_ght[i] = WN;
+  }
+
+  custom_globalHistory = 0;
+
+  uint16_t custom_lhtEntries = 1 << custom_localHistoryBits;
+  custom_localHistory = (uint16_t *)malloc(custom_lhtEntries * sizeof(uint16_t));
+  custom_localPredTable = (uint8_t *)malloc(custom_lhtEntries * sizeof(uint8_t));
+
+  for(int i = 0; i < custom_lhtEntries; i++)
+  {
+    custom_localPredTable[i] = VWNT;
+  }
+
+  for(int i = 0; i < custom_lhtEntries; i++)
+  {
+    custom_localHistory[i] = 0xAAAA;
+  }
+
+  custom_chooser = (uint8_t *)malloc(custom_ghtEntries * sizeof(uint8_t));
+
+  for(int i = 0; i < custom_ghtEntries; i++)
+  {
+    custom_chooser[i] = 1;
+  }
+
+}
+
+uint8_t custom_predictGlobal()
+{
+  uint16_t custom_ghtEntries  = 1 << custom_globalHistoryBits;
+  uint16_t index = custom_globalHistory & (custom_ghtEntries - 1);
+  
+  switch (custom_ght[index])
+  {
+  case WN:
+    return NOTTAKEN;
+  case SN:
+    return NOTTAKEN;
+  case WT:
+    return TAKEN;
+  case ST:
+    return TAKEN;
+  default:
+    printf("Warning: Undefined state of entry in custom BHT!\n");
+    return NOTTAKEN;
+  }
+}
+
+uint8_t custom_predictLocal(uint32_t pc)
+{
+  uint32_t custom_lhtentries = 1 << custom_localHistoryBits;
+  uint32_t pc_lower_bits = (pc & (custom_lhtentries - 1));
+  uint16_t history = custom_localHistory[pc_lower_bits];
+  uint32_t index = (history & (custom_lhtentries - 1));
+
+  switch(custom_localPredTable[index])
+  {
+    case VSNT:
+      return NOTTAKEN;
+    case SNT:
+      return NOTTAKEN;
+    case WNT:
+      return NOTTAKEN;
+    case VWNT:
+      return NOTTAKEN;
+    case VWT:
+      return TAKEN;
+    case WTT:
+      return TAKEN;
+    case STT:
+      return TAKEN;
+    case WST:
+      return TAKEN;
+    default:
+      return NOTTAKEN;
+  }
+}
+
+uint8_t custom_predict(uint32_t pc)
+{
+  uint8_t globalPred = custom_predictGlobal();
+  uint8_t localPred = custom_predictLocal(pc);
+
+  uint16_t custom_ghtEntries  = 1 << custom_globalHistoryBits;
+  uint16_t index = custom_globalHistory & (custom_ghtEntries - 1);
+
+  uint8_t finalPred;
+
+  if(custom_chooser[index] >= 2)
+  {
+    finalPred = globalPred;
+  }
+  else
+  {
+    finalPred = localPred;
+  }
+
+  return finalPred;
+}
+
+uint8_t custom_trainGlobal(uint8_t outcome)
+{
+  // Training Global Prediction
+  uint16_t custom_ghtEntries  = 1 << custom_globalHistoryBits;
+  uint16_t index = custom_globalHistory & (custom_ghtEntries - 1);
+  uint8_t retVal;
+
+  // Update state of entry in bht based on outcome
+  switch (custom_ght[index])
+  {
+  case WN:
+  {
+    custom_ght[index] = (outcome == TAKEN) ? WT : SN;
+    retVal = NOTTAKEN;
+    break;
+  }
+  case SN:
+  {
+    custom_ght[index] = (outcome == TAKEN) ? WN : SN;
+    retVal = NOTTAKEN;
+    break;
+  }
+  case WT:
+  {
+    custom_ght[index] = (outcome == TAKEN) ? ST : WN;
+    retVal = TAKEN;
+    break;
+  }
+  case ST:
+  {
+    custom_ght[index] = (outcome == TAKEN) ? ST : WT;
+    retVal = TAKEN;
+    break;
+  }
+  default:
+    printf("Warning: Undefined state of entry in custom GHT!\n");
+    break;
+  }
+
+  custom_globalHistory = ((custom_globalHistory << 1) | outcome);
+  return retVal;
+}
+
+uint8_t custom_trainLocal(uint32_t pc , uint8_t outcome)
+{
+  uint32_t custom_lhtentries = 1 << custom_localHistoryBits;
+  uint32_t pc_lower_bits = (pc & (custom_lhtentries - 1));
+  uint32_t history = custom_localHistory[pc_lower_bits];
+  uint32_t index = (history & (custom_lhtentries - 1));
+
+  uint8_t retVal;
+
+  switch (custom_localPredTable[index])
+  {
+  case VSNT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? SNT : VSNT;
+    retVal = NOTTAKEN;
+    break;
+  } 
+  case SNT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? WNT : VSNT;
+    retVal = NOTTAKEN;
+    break;
+  }
+  case WNT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ?  VWNT : SNT;
+    retVal = NOTTAKEN;
+    break;
+  }
+  case VWNT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? VWT : WNT;
+    retVal = NOTTAKEN;
+    break;
+  }
+  case VWT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? WTT : VWNT;
+    retVal = TAKEN;
+    break;
+  }
+  case WTT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? STT : VWT;
+    retVal = TAKEN;
+    break;
+  }
+  case STT:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? WST : WTT;
+    retVal = TAKEN;
+    break;
+  }
+  case WST:
+  {
+    custom_localPredTable[index] = (outcome == TAKEN) ? WST : STT;
+    retVal = TAKEN;
+    break;
+  }
+    
+  default:
+    printf("Warning: Undefined state of entry in custom LHT!\n");
+    break;
+  }
+
+  custom_localHistory[pc_lower_bits] = ((custom_localHistory[pc_lower_bits] << 1) | outcome);
+
+  return retVal;
+}
+
+void custom_train(uint32_t pc, uint8_t outcome)
+{
+  uint16_t custom_ghtEntries  = 1 << custom_globalHistoryBits;
+  uint16_t index = custom_globalHistory & (custom_ghtEntries - 1);
+
+  //uint8_t globTrained = custom_trainGlobal(outcome);
+  uint8_t globTrained = custom_trainGlobal(outcome);
+  uint8_t localTrained = custom_trainLocal(pc, outcome);
+
+  if(globTrained == localTrained)
+  {
+    // Do nothing
+  }
+  else if(globTrained == outcome)
+  {
+    if(custom_chooser[index] != 3)
+    {
+      custom_chooser[index]++;
+    }
+  }
+  else if(localTrained == outcome)
+  {
+    if(custom_chooser[index] != 0)
+    {
+      custom_chooser[index]--;
+    }
+  }
+}
+
+void custom_free()
+{
+  free(custom_ght);
+  free(custom_localHistory);
+  free(custom_localPredTable);
+  free(custom_chooser);
+}
+
+
 
 void init_predictor()
 {
@@ -406,6 +674,7 @@ void init_predictor()
     a21264_init();
     break;
   case CUSTOM:
+    custom_init();
     break;
   default:
     break;
@@ -429,7 +698,7 @@ uint32_t make_prediction(uint32_t pc, uint32_t target, uint32_t direct)
   case TOURNAMENT:
     return a21264_predict(pc);
   case CUSTOM:
-    return NOTTAKEN;
+    return custom_predict(pc);
   default:
     break;
   }
@@ -456,7 +725,7 @@ void train_predictor(uint32_t pc, uint32_t target, uint32_t outcome, uint32_t co
     case TOURNAMENT:
       return a21264_train(pc, outcome);
     case CUSTOM:
-      return;
+      return custom_train(pc, outcome);
     default:
       break;
     }
